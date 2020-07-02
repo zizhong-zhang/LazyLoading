73f3e2fb6bffda205081d585881b14dbb1afce84
Zizhong Zhang <zizhong.zhang43@gmail.com>
2020-07-02 20:24:08 +1000

    Add project files.

---
+++ Web.Core/Controllers/GuidGeneratorController.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GuidGeneratorController : ControllerBase
+    {
+        private readonly IGuidGenerator _guidGenerator;
+
+        public GuidGeneratorController(IGuidGenerator guidGenerator)
+        {
+            _guidGenerator = guidGenerator;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_guidGenerator.GetGuid());
+        }
+    }
+}
